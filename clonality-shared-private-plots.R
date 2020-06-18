adea949cfba6a6b942ab28f769664aa4efa752e1
Shweta Chavan <chavan.shweta@gmail.com>
2020-06-17 23:35:40 -0400

    Update clonality-shared-private-plots.R

--- clonality-shared-private-plots.R
+++ clonality-shared-private-plots.R
@@ -3,7 +3,37 @@
 #### Clonality plot ####
 ######################################
 #~/Rscript3.5.0 ~/res/scripts-orphan/get_ccf_subclonality_all_batches_expected_copies.R current_wes_somatic50.maf current_wes_somatic50.ccf.maf
+######
+##Clinical track
+cli_info<-fread("~/bladder-paper/wes_clinical.csv")
+status_order<-c("met.location","type.treatment","time.between")
+cli_info$key=factor(cli_info$key,level=c(unique(cli_info$key)))
+cli_info$value=factor(cli_info$value,level=c(unique(cli_info$value)))
+cli_info$patient.id<-factor(cli_info$patient.id)
+patient.order<-cli_info$patient.id
 
+color_map<- c("#D9D9D9","#969696", "#FFF7EC","#FDD49E","#FC8D59","#D7301F","#7F0000","#DEEBF7" ,"#9ECAE1", "#4292C6", "#08519C")
+track<- ggplot(cli_info, aes(x=patient.id,y=key))+ 
+  geom_tile(aes(fill=value),width=0.9,height=0.9)+
+  scale_y_discrete(limits=rev(status_order))+
+  scale_x_discrete(limits=patient.order) +
+  scale_fill_manual(values=color_map,name = "Clinical Information",guide="legend")+ 
+  guides(color = guide_legend(ncol = 1)) +
+  theme(legend.key = element_rect(size = 2, color = "white"),
+        legend.key.size = unit(1.5, "lines"))+
+  theme(text = element_text(color = "gray20"),
+        legend.position = c("right"), # position the legend in the upper left
+        legend.justification = 0, # anchor point for legend.position.
+        legend.text = element_text(size = 12, color = "gray10"),
+        title = element_text(size = 12, face = "bold", color = "gray10"),
+        axis.text = element_text(size =12, face = "bold",angle=90),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank()
+  )
+track
+##############################
+##############################
 #wesclonalitymaf = fread('/Users/chavans/juno/work/ccs/ccs_wes/Proj_07871_DFLOQ/Result/somatic/allzygositymafs.unique.clonality.maf')
 wesclonalitymaf0 =  fread('~/bladder-paper/bladder_mut_somatic.ccf.freeze.050520.maf')
 dim(wesclonalitymaf0)
